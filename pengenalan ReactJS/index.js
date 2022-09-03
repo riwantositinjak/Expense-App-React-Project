99ed924b19582d442cd8e86da19772a752501225
riwantositinjak <riwanto1212@gmail.com>
2022-09-04 04:58:13 +0700

    materi pembelajaran ReactJS

---
+++ pengenalan ReactJS/index.js
@@ -0,0 +1,11 @@
+// program pertama menampilkan Hello World di ReactJS
+// ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById("root"));
+
+// menampilkan Unorderlist HTML di reactJS
+ReactDOM.render(
+  <ul>
+    <li>List 1</li>
+    <li>List 2</li>
+  </ul>,
+  document.getElementById("root")
+);
