b9764ce7096ad260690d6a7ce8ebe8d962fb910c
riwantositinjak <riwanto1212@gmail.com>
2022-09-04 17:44:40 +0700

    menambahkan package React ke Folder Pembelajaran React

---
+++ React 3 JSX/index.js
@@ -0,0 +1,41 @@
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+// JSX
+// const page = (
+//   <div>
+//     <h1 className="header">This is JSX</h1>
+//     <p>This is a paragraph</p>
+//   </div>
+// );
+
+// console.log(page);
+
+// ReactDOM.render(page, document.getElementById("root"));
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navigationBar = (
+  <nav>
+    <h1>Bob's Bistro</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navigationBar, document.getElementById("root"));
