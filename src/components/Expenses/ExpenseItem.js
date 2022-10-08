47786bece9451e9de65efae478ef07ea466ea275
riwantositinjak <riwanto1212@gmail.com>
2022-10-08 22:42:21 +0700

    update some materi

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,26 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const eventHandler = () => {
+    setTitle("Update Title");
+    console.log(title)
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={() => console.log("tombol ini di klik!")}>
-        Change title
-      </button>
+      <button onClick={eventHandler}>Change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
